634399cd4af3956c5610ddedf0cf367b0b8c8356
rajeshk <kuruvarajesh607@gmail.com>
2023-07-23 08:49:35 +0530

    dashboard code added

--- rajesh-app/src/components/PieChart/Barchart.js
+++ rajesh-app/src/components/PieChart/Barchart.js
@@ -3,53 +3,46 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: 'Sat',
+    Debit:350,
+    Credit:600
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: 'Sun',
+    Debit:250,
+    Credit:500
   },
   {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: 'Mon',
+    Debit:250,
+    Credit:370
   },
   {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: 'Tue',
+    Debit:570,
+    Credit:350
   },
   {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: 'Wed',
+    Debit:400,
+    Credit:550
   },
   {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    name: 'Thu',
+    Debit:450,
+    Credit:250
   },
   {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: 'Fri',
+    Debit:390,
+    Credit:490
   },
 ];
 
 const Barchart = (props)=>{
  
     return (
-      <ResponsiveContainer width={1056} height={364} border="1px solid red">
+      <ResponsiveContainer width={1356} height={364} border="1px solid red">
         <BarChart
           width={500}
           height={300}
@@ -66,9 +59,8 @@ const Barchart = (props)=>{
           <YAxis  />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-          <Bar dataKey="amt" stackId="a" fill="#82ca9d"  />
-          <Bar dataKey="uv" fill="#ffc658" />
+          <Bar dataKey="Debit" stackId="a" fill="#4D78FF" />
+          <Bar dataKey="Credit" fill="#FCAA0B" />
         </BarChart>
       </ResponsiveContainer>
      
