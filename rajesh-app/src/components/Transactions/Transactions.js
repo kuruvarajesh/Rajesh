0eb62b7b5ff185947542bc3158ffba63ee669c4c
rajeshk <kuruvarajesh607@gmail.com>
2023-08-06 08:12:23 +0530

    add user id in transaction

--- rajesh-app/src/components/Transactions/Transactions.js
+++ rajesh-app/src/components/Transactions/Transactions.js
@@ -35,7 +35,7 @@ const getAllTransactions = async()=>{
         "content-type":"application/json",
         "x-hasura-admin-secret":"g08A3qQy00y8yFDq3y6N1ZQnhOPOa4msdie5EtKS1hFStar01JzPKrtKEzYY2BtF",
         "x-hasura-role":"user",
-        "x-hasura-user-id":"1"
+        "x-hasura-user-id":accessToken.toString()
       }
       const adminHeaders = {
         "content-type":"application/json",
