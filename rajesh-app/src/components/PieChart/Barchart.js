4e738bbd29d69fc2e60482c7f4afbed78580d591
kuruvarajesh <kuruvarajesh607@gmail.com>
2023-10-09 10:03:18 +0530

    bar chart code updated

--- rajesh-app/src/components/PieChart/Barchart.js
+++ rajesh-app/src/components/PieChart/Barchart.js
@@ -1,83 +1,61 @@
-import React, { PureComponent } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import {useJsonPlaceHolder} from 'react-fakers'
 
-const data = [
-  {
-    name: 'Sat',
-    Debit:350,
-    Credit:600
-  },
-  {
-    name: 'Sun',
-    Debit:250,
-    Credit:500
-  },
-  {
-    name: 'Mon',
-    Debit:250,
-    Credit:370
-  },
-  {
-    name: 'Tue',
-    Debit:570,
-    Credit:350
-  },
-  {
-    name: 'Wed',
-    Debit:400,
-    Credit:550
-  },
-  {
-    name: 'Thu',
-    Debit:450,
-    Credit:250
-  },
-  {
-    name: 'Fri',
-    Debit:390,
-    Credit:490
-  },
-];
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+let numbers = [100, 200, 300, 400,500]
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      
 
-const Barchart = (props)=>{
- 
-    return (
-      <ResponsiveContainer width={"100%"} height={364} >
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis  
-            tickCount={5}
-            tick={{
-              stroke: "gray",
-              strokeWidth: 0,
-            }}
-            ticks={[100, 200, 300, 400, 500]}/>
-          <Tooltip />
-          <Legend  
-          content={"hello nlaewn wjr jr'ilwj wrj ioqwr' r'oih 'iof'w4 'o4w rhrowjr'owjf'oewhf;awonv;iaehg oaegaeri;ghiaeroah"}
-            layout="horizontal" // Change the layout to horizontal to display the legend items in a row
-            verticalAlign="top" // Position the legend at the top of the chart
-            align="right" // Align the legend items to the center
-             />
-             <CartesianGrid vertical={false} /> {/* Set vertical prop to false */}
-          <Bar dataKey="Debit" stackId="a" fill="#4D78FF" />
-          <Bar dataKey="Credit" fill="#FCAA0B" />
-        </BarChart>
-      </ResponsiveContainer>
-     
-    );
-  
+    },
+    title: {
+      display: true,
+      text: '$7,560 Debited & $5,420 Credited in this Week',
+    },
+    ticks:{numbers},
+  },
+};
+
+const labels = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+
+ const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Debit',
+      data: labels.map(() => Math.floor(Math.random()*(100-1)+1)),
+      backgroundColor: '#4D78FF',
+    },
+    {
+      label: 'Credit',
+      data: labels.map(() => Math.floor(Math.random()*(100-1)+1)),
+      backgroundColor: '#FCAA0B',
+    },
+  ],
+};
+
+function Barchart() {
+  return <Bar options={options} width={900} height={234} data={data} />;
 }
 
 export default Barchart
